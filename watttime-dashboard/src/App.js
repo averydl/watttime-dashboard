dcc8eb9bd8271957745eaef5f5f6808b847eff0e
Matthew Clark <everdein@gmail.com>
2020-04-29 12:42:50 -0700

    Getting Started
    Setup environment to work in and added sidebar.

---
+++ watttime-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FilterWindow from './components/FilterWindow'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+        {/* <h1>hello world</h1> */}
+        <FilterWindow/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
