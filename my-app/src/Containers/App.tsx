21f9cec0cf07cdbc0cb89e6bd58c35aaf58d1962
Beau BCC <beau.shirdavani@bellevuecollege.edu>
2020-05-08 13:51:30 -0700

    added type script demo block to App.tsx...delete this later

--- my-app/src/Containers/App.tsx
+++ my-app/src/Containers/App.tsx
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //-----------------------------------------------
+  // demo block for using types in TypeScript
+  // TODO: delete this block later
+  const testStringConstant: string = "string constant";
+  let testNotConstant: any = "I was a string, but my type = any";
+  console.log(testNotConstant);
+  testNotConstant = 1;
+  testNotConstant = "Now I'm a string again";
+  //-----------------------------------------------
   return (
     <div className="App">
       <div>Placeholder for MENU</div>
