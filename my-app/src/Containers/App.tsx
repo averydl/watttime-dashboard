bdf7ce946ca70e9449c561315a0fecbb157871b6
Beau BCC <beau.shirdavani@bellevuecollege.edu>
2020-05-08 13:38:30 -0700

    structured folders - Containerts holds components that will change state/data, Components holds components that just display stuff.

---
+++ my-app/src/Containers/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Placeholder for MENU</div>
+      <div>Placeholder for CHART 1</div>
+      <div>Placeholder for CHART 2</div>
+    </div>
+  );
+}
+
+export default App;
