8d55a4ce14ffce69e5c069a1c8f67140457bf913
Beau BCC <beau.shirdavani@bellevuecollege.edu>
2020-05-11 19:26:17 -0700

    failed date time labels...d3.timeFormat shows the wrong date.

--- my-app/src/Components/Charts/ChartLines.tsx
+++ my-app/src/Components/Charts/ChartLines.tsx
@@ -3,17 +3,29 @@ import dummyData from '../../DummyData/power.json';
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   } from 'recharts';
+import * as d3 from 'd3';
+
 
 class ChartLines extends Component {
 
     data: any = dummyData.powerData;
 
     render(){
-        console.log(dummyData);
+        console.log(this.data);
+
+        this.data.forEach((element: any) => {
+            console.log(element.dateTime);
+            element.date = new Date(element.dateTime);
+            console.log(element.date);
+        });
+
+
         return(
             <div>
                 <LineChart width={600} height={300} data={this.data}
                 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    {/* <XAxis dataKey="dateTime" tickFormatter={d3.timeParse('%Y-%m-%d %H:%M:%S_Z')}/> */}
+                    {/* <XAxis dataKey="date" tickFormatter={d3.timeFormat('%Y-%m-%d')}/> */}
                     <XAxis dataKey="dateTime"/>
                     <YAxis/>
                     <CartesianGrid strokeDasharray="3 3"/>
