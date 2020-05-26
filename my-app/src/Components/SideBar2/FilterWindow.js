5a485213e7119e55bbd4b535502d6fb00679e184
Matthew Clark <everdein@gmail.com>
2020-05-26 15:49:43 -0700

    Sidebar Updated
    Updated side bar to store date range and balancing authority.

--- my-app/src/Components/SideBar2/FilterWindow.js
+++ my-app/src/Components/SideBar2/FilterWindow.js
@@ -1,93 +1,34 @@
 import React from 'react'
-// import '..\\src\\Containers\\App.css'
 
-
+// import React, { Component } from 'react';
+// import Calendar from 'react-calendar';
 
 class FilterWindow extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      // Search bar
-      searchInput: '',
-
-      // Gender Checkboxes
-      isMale: false,
-      isFemale: false,
-      isOther: false,
-
-      // Amenities Checkboxes
-      isAdaAccessible: false,
-      isFeminineHygiene: false,
-      isDoorCode: false,
-      isContraceptives: false,
-      isFamily: false,
-
-      // Star rating
-      selectedValue: 'SortByRating',
-      isStarRating: null,
-    }
-  }
-
-  // Search bar
-  handleSearchBar = (e) => {
-    this.setState({ searchInput: e.target.value })
-    console.log(e.target.value)
-  }
-
-  onSubmitSearchBar = (e) => {
-    e.preventDefault()
-    console.log(this.state.searchInput)
-  }
-
-  // Gender Checkboxes
-  handleGenderCheckboxChange = (e) => {
-    const target = e.target
-    if (target.name === 'male') {
-      console.log(target.checked + ' male')
-      this.setState({ isMale: true })
-    }
-    if (target.name === 'female') {
-      console.log(target.checked + ' female')
-      this.setState({ isFemale: true })
-    }
-    if (target.name === 'other') {
-      console.log(target.checked + ' other')
-      this.setState({ isOther: true })
-    }
-  }
-
-  // Amenities Checkboxes
-  handleAmenitiesCheckboxes = (e) => {
-    const target = e.target
-    if (target.name === 'adaAccessible') {
-      console.log(target.checked + ' adaAccessible')
-      this.setState({ isAdaAccessible: true })
-    }
-    if (target.name === 'feminineHygiene') {
-      console.log(target.checked + ' feminineHygiene')
-      this.setState({ isFeminineHygiene: true })
-    }
-    if (target.name === 'doorCode') {
-      console.log(target.checked + ' doorCode')
-      this.setState({ isDoorCode: true })
-    }
-    if (target.name === 'contraceptives') {
-      console.log(target.checked + ' contraceptives')
-      this.setState({ isContraceptives: true })
-    }
-    if (target.name === 'family') {
-      console.log(target.checked + ' family')
-      this.setState({ isFamily: true })
+      selectedDateRange: 'sortByDateRange',
+      selectedBalancingAuthority: 'sortByBalancingAuthority',
+      // date: new Date(),
     }
   }
 
   // Drop down
-  handleDropDownValue = (e) => {
+  handleDropDownDateRange = (e) => {
     this.setState({
-      selectedValue: e.target.value,
+      selectedDateRange: e.target.value,
     })
-    console.log('You have submitted:', e.target.value)
+    console.log('You have submitted: ', e.target.value, ' day(s).')
   }
+
+  handleDropDownBalancingAuthority = (e) => {
+    this.setState({
+      selectedBalancingAuthority: e.target.value,
+    })
+    console.log('You have submitted: ', e.target.value, ' balancing authority.')
+  }
+
+  // onChange = date => this.setState({ date })
 
   // Apply filters
   applyFilters = (e) => {
@@ -99,126 +40,114 @@ class FilterWindow extends React.Component {
     return (
       /* Navigation Bar */
       <div className='sidebar-navigation'>
-        {/* Search Bar */}
-        <div className='search-bar'>
-          <form onSubmit={(e) => this.onSubmitSearchBar(e)}>
-            <input
-              type='search-bar'
-              className='input'
-              id='addInput'
-              placeholder='Location'
-              onChange={(e) => this.handleSearchBar(e)}
-            />
-            <br></br><br></br>
-            <button submit='submit' class='block'>Search</button>
-          </form>
-        </div>
-        <br></br>
-
         {/* Apply Filters Form */}
         <form onSubmit={(e) => this.applyFilters(e)}>
-          {/* Gender Checkboxes */}
-          <div className='gender-checkboxes'>
-            <input
-              name='male'
-              id='male'
-              type='checkbox'
-              checked={this.state.male}
-              onChange={(e) => this.handleGenderCheckboxChange(e)}
-            />
-            <label htmlFor='male'>Male</label>
-            <br></br>
-            <input
-              name='female'
-              id='female'
-              type='checkbox'
-              checked={this.state.male}
-              onChange={(e) => this.handleGenderCheckboxChange(e)}
-            />
-            <label htmlFor='female'>Female</label>
-            <br></br>
-
-            <input
-              name='other'
-              id='other'
-              type='checkbox'
-              checked={this.state.male}
-              onChange={(e) => this.handleGenderCheckboxChange(e)}
-            />
-            <label htmlFor='other'>Other</label>
-          </div>
-          <br></br>
-
-          {/* Amenities Checkboxes */}
-          <div className='amenities-checkboxes'>
-            <input
-              name='adaAccessible'
-              id='adaAccessible'
-              type='checkbox'
-              checked={this.state.adaAccessible}
-              onChange={(e) => this.handleAmenitiesCheckboxes(e)}
-            />
-            <label htmlFor='adaAccessible'>Ada Accessible</label>
-            <br></br>
-
-            <input
-              name='feminineHygiene'
-              id='feminineHygiene'
-              type='checkbox'
-              checked={this.state.feminineHygiene}
-              onChange={(e) => this.handleAmenitiesCheckboxes(e)}
-            />
-            <label htmlFor='feminineHygien'>Feminine Hygiene</label>
-            <br></br>
-
-            <input
-              name='doorCode'
-              id='doorCode'
-              type='checkbox'
-              checked={this.state.doorCode}
-              onChange={(e) => this.handleAmenitiesCheckboxes(e)}
-            />
-            <label htmlFor='doorCode'>Door Code</label>
-            <br></br>
-
-            <input
-              name='contraceptives'
-              id='contraceptives'
-              type='checkbox'
-              checked={this.state.contraceptives}
-              onChange={(e) => this.handleAmenitiesCheckboxes(e)}
-            />
-            <label htmlFor='contraceptive'>Contraceptives</label>
-            <br></br>
-
-            <input
-              name='family'
-              id='family'
-              type='checkbox'
-              checked={this.state.family}
-              onChange={(e) => this.handleAmenitiesCheckboxes(e)}
-            />
-            <label htmlFor='contraceptives'>Family</label>
-          </div>
-          <br></br>
-
-          {/* Star Ratings Drop Down Menu */}
+          
+          {/* Sort By Date Range */}
           <div className='drop-down'>
             <select
               class='block'
-              value={this.state.selectedValue}
-              onChange={(e) => this.handleDropDownValue(e)}
-              id='sortByRating'
+              value={this.state.selectedDateRange}
+              onChange={(e) => this.handleDropDownDateRange(e)}
+              id='sortByDateRange'
             >
-              <option value='sortByRating'>Sort by Rating</option>
-              <option value='1 Star'>1 Star</option>
-              <option value='2 Star'>2 Star</option>
-              <option value='3 Star'>3 Star</option>
-              <option value='4 Star'>4 Star</option>
-              <option value='5 Star'>5 Star</option>
+              <option value='sortByDateRange'>Date Range</option>
+              <option value='1'>1 Day</option>
+              <option value='2'>2 Days</option>
+              <option value='3'>3 Days</option>
+              <option value='4'>4 Days</option>
+              <option value='5'>5 Days</option>
+              <option value='6'>6 Days</option>
+              <option value='7'>7 Days</option>
             </select>
           </div>
           <br></br>
+
+          {/* Sort By Balancing Authority */}
+          <div className='drop-down-balancing-authority'>
+            <select
+              class='block'
+              value={this.state.selectedBalancingAuthority}
+              onChange={(e) => this.handleDropDownBalancingAuthority(e)}
+              id='sortByBalancingAuthority'
+            >
+              <option value='sortByBalancingAuthority'>Balancing Authority</option>
+              <option value='YAD'>YAD</option>
+              <option value='DEAA'>DEAA</option>
+              <option value='AECI'>AECI</option>
+              <option value='AVRN'>AVRN</option>
+              <option value='AVA'>AVA</option>
+              <option value='BANC'>BANC</option>
+              <option value='BPAT'>BPAT</option>
+              <option value='CISO'>CISO</option>
+              <option value='HST'>HST</option>
+              <option value='TPWR'>TPWR</option>
+              <option value='TAL'>TAL</option>
+              <option value='DUK'>DUK</option>
+              <option value='FPC'>FPC</option>
+              <option value='CPLE'>CPLE</option>
+              <option value='CPLW'>CPLW</option>
+              <option value='EPE'>EPE</option>
+              <option value='EEI'>EEI</option>
+              <option value='ERCO'>ERCO</option>
+              <option value='FMPP'>FMPP</option>
+              <option value='FPL'>FPL</option>
+              <option value='GVL'>GVL</option>
+              <option value='GLHB'>GLHB</option>
+              <option value='GRID'>GRID</option>
+              <option value='GRIF'>GRIF</option>
+              <option value='ISNE'>ISNE</option>
+              <option value='IPCO'>IPCO</option>
+              <option value='IID'>IID</option>
+              <option value='JEA'>JEA</option>
+              <option value='LDWP'>LDWP</option>
+              <option value='LGEE'>LGEE</option>
+              <option value='MISO'>MISO</option>
+              <option value='GWA'>GWA</option>
+              <option value='WWA'>WWA</option>
+              <option value='NEVP'>NEVP</option>
+              <option value='HGMA'>HGMA</option>
+              <option value='NYIS'>NYIS</option>
+              <option value='NWMT'>NWMT</option>
+              <option value='OVEC'>OVEC</option>
+              <option value='PJM'>PJM</option>
+              <option value='DOPD'>DOPD</option>
+              <option value='PACE'>PACE</option>
+              <option value='PACW'>PACW</option>
+              <option value='PGE'>PGE</option>
+              <option value='AEC'>AEC</option>
+              <option value='PSCO'>PSCO</option>
+              <option value='PNM'>PNM</option>
+              <option value='CHPD'>CHPD</option>
+              <option value='GCPD'>GCPD</option>
+              <option value='PSEI'>PSEI</option>
+              <option value='SRP'>SRP</option>
+              <option value='SCL'>SCL</option>
+              <option value='SEC'>SEC</option>
+              <option value='SCEG'>SCEG</option>
+              <option value='SC'>SC</option>
+              <option value='SEPA'>SEPA</option>
+              <option value='SOCO'>SOCO</option>
+              <option value='SWPP'>SWPP</option>
+              <option value='SPA'>SPA</option>
+              <option value='TEC'>TEC</option>
+              <option value='TVA'>TVA</option>
+              <option value='TEPC'>TEPC</option>
+              <option value='TIDC'>TIDC</option>
+              <option value='NSB'>NSB</option>
+              <option value='WALC'>WALC</option>
+              <option value='WACM'>WACM</option>
+              <option value='WAUW'>WAUW</option>
+            </select>
+          </div>
+          <br></br>
+
+          {/* <div>
+            <Calendar
+              onChange={this.onChange}
+              value={this.state.date}
+            />
+          </div> */}
 
           {/* Apply Filters Button */}
           <button type='submit' class='block'>
